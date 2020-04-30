48db39b3cf967f7b9e8fecea578e5a59cddbe8de
Sreelakshmi <ppsree1790@gmail.com>
2020-04-30 23:32:12 +0530

    Adding routing to the App -for Home, About, Cards

--- ecard-app/src/app/app-routing.module.ts
+++ ecard-app/src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from 'src/app/home/home.component';
+import { AboutComponent } from 'src/app/about/about.component';
+import { CardsComponent } from 'src/app/cards/cards.component';
+import { CardDetailComponent } from 'src/app/card-detail/card-detail.component';
+import { CardPreviewComponent } from 'src/app/card-preview/card-preview.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "" ,redirectTo: "home", pathMatch:"full" },
+  {path: "home", component: HomeComponent},
+  {path: "about", component: AboutComponent},
+  {path: "cards", component: CardsComponent}
+  // {path: "cards/id", component: CardDetailComponent},
+  // {path: "cards/id/preview", component: CardPreviewComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
