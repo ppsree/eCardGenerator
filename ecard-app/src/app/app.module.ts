243f0a3c906a3dc9d0737b007b9d9077f61e1986
Sreelakshmi <ppsree1790@gmail.com>
2020-04-30 23:05:37 +0530

    Add all required components

--- ecard-app/src/app/app.module.ts
+++ ecard-app/src/app/app.module.ts
@@ -5,11 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProgressTrackerComponent } from './progress-tracker/progress-tracker.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { AboutComponent } from './about/about.component';
+import { CardDetailComponent } from './card-detail/card-detail.component';
+import { CardPreviewComponent } from './card-preview/card-preview.component';
+import { CardsComponent } from './cards/cards.component';
+import { TagComponent } from './tag/tag.component';
+import { CardSummaryComponent } from './card-summary/card-summary.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProgressTrackerComponent
+    ProgressTrackerComponent,
+    HomeComponent,
+    MenuComponent,
+    AboutComponent,
+    CardDetailComponent,
+    CardPreviewComponent,
+    CardsComponent,
+    TagComponent,
+    CardSummaryComponent
   ],
   imports: [
     BrowserModule,
---
+++ ecard-app/src/app/card-preview/card-preview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-preview',
+  templateUrl: './card-preview.component.html',
+  styleUrls: ['./card-preview.component.css']
+})
+export class CardPreviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
