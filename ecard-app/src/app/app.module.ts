589b4e06f2f56dfcf84cbaa49355649f9300213d
Sreelakshmi <ppsree1790@gmail.com>
2020-04-30 21:30:55 +0530

    Incldue Angular Material Package

--- ecard-app/src/app/app.module.ts
+++ ecard-app/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProgressTrackerComponent } from './progress-tracker/progress-tracker.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { ProgressTrackerComponent } from './progress-tracker/progress-tracker.co
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
