3dd976323d275db47de0992855ebc62516d12e83
Sreelakshmi <ppsree1790@gmail.com>
2020-04-30 17:37:46 +0530

    Adding progress tracker component and JSON file.

--- ecard-app/src/app/app.module.ts
+++ ecard-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProgressTrackerComponent } from './progress-tracker/progress-tracker.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProgressTrackerComponent
   ],
   imports: [
     BrowserModule,
---
+++ ecard-app/src/app/progress-tracker/progress-tracker.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-progress-tracker',
+  templateUrl: './progress-tracker.component.html',
+  styleUrls: ['./progress-tracker.component.css']
+})
+export class ProgressTrackerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
