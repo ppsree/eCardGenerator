1334b21853107cc822e69a9206a3bc517213b868
Sreelakshmi <ppsree1790@gmail.com>
2020-05-04 23:18:29 +0530

    Added grid view to Cards Page

--- ecard-app/src/app/cards/cards.component.ts
+++ ecard-app/src/app/cards/cards.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CardsService } from '../cards.service';
 
 @Component({
   selector: 'app-cards',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardsComponent implements OnInit {
 
-  constructor() { }
+  public cards: any;
+  public tags: Array<string> = [];
+  constructor(private router: Router,
+    private cardsService: CardsService) { }
 
   ngOnInit() {
+    this.cardsService.getCards().subscribe(
+      res => {
+        this.cards = res;
+        for (let card of this.cards) {
+          if (this.tags.indexOf(card.tags) == -1) {
+            this.tags.push(card.tags);
+          }
+        }
+      }
+    );
+    
+  }
+
+  navigateTo(id: string){
+    this.router.navigate(['/card',id]);
+  }
+
+  filterCards(event) {
+    console.log(event);
+    for (let card in this.cards) {
+      if (card["tags"] != event) {
+        
+      }
+    }
   }
 
 }
