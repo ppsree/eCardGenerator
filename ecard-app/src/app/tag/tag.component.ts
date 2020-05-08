43582fe526300c2c9011764756cc0eb702db05c9
Sreelakshmi <ppsree1790@gmail.com>
2020-05-08 19:30:49 +0530

    Added tag filtering

--- ecard-app/src/app/tag/tag.component.ts
+++ ecard-app/src/app/tag/tag.component.ts
@@ -10,13 +10,40 @@ export class TagComponent implements OnInit {
 
   @Input() tags: Array<string>;
   @Output() filterSelected = new EventEmitter();
-  constructor() { }
+  public clr: string = "primary";
+  public tagJson = {};
+
+  constructor() { 
+    
+  }
 
   ngOnInit() {
+    window.localStorage.removeItem('tags');
+  }
+
+  ngAfterViewChecked() {
+    if (window.localStorage.getItem('tags') && window.localStorage.getItem('tags').toString() != '') {
+      // this.tagJson = JSON.parse(window.localStorage.getItem('tags'));
+    } else {
+      for (let tag of this.tags) {
+        this.tagJson[tag] = 'false';
+        window.localStorage.setItem('tags',JSON.stringify(this.tagJson));
+      } 
+    }
+    
   }
 
   filterCards(tag: string) {
-    this.filterSelected.emit(tag);
+    
+    if (this.tagJson[tag] == 'false') {
+      this.tagJson[tag] = 'true';
+      this.clr = "accent";
+    } else {
+      this.tagJson[tag] = 'false';
+      this.clr = "primary";
+    }
+    window.localStorage.setItem('tags',JSON.stringify(this.tagJson));
+    this.filterSelected.emit(this.tagJson);
   }
 
 }
